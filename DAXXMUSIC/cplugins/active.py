b6a52f5d358b4703c5c895f4cfc63f1a00297382
Anik <135734362+anikalwayspro@users.noreply.github.com>
2024-05-08 00:07:38 +0530

    Update active.py

--- DAXXMUSIC/cplugins/active.py
+++ DAXXMUSIC/cplugins/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from unidecode import unidecode
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import (
+from DAXXMUSIC import app
+from DAXXMUSIC.misc import SUDOERS
+from DAXXMUSIC.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
