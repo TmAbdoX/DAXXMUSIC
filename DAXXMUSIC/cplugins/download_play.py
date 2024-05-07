7da4230cd23b30eba05b3380f275848a6bd97570
Anik <135734362+anikalwayspro@users.noreply.github.com>
2024-05-08 00:10:44 +0530

    Update download_play.py

--- DAXXMUSIC/cplugins/download_play.py
+++ DAXXMUSIC/cplugins/download_play.py
@@ -15,7 +15,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-from VIPMUSIC import app
+from DAXXMUSIC import app
 import asyncio
 import os
 import time
@@ -25,10 +25,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-from VIPMUSIC import app
+from DAXXMUSIC import app
 from time import time
 import asyncio
-from VIPMUSIC.utils.extraction import extract_user
+from DAXXMUSIC.utils.extraction import extract_user
 import asyncio
 import os
 import wget
@@ -37,10 +37,10 @@ from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 
-from VIPMUSIC import app
-from VIPMUSIC.utils.extraction import extract_user
+from DAXXMUSIC import app
+from DAXXMUSIC.utils.extraction import extract_user
 from time import time
-from VIPMUSIC.utils.extraction import extract_user
+from DAXXMUSIC.utils.extraction import extract_user
 from urllib.parse import urlparse
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters, Client
